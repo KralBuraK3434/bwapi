1f504393f7e4eba29815fa68c99ad5767395603e
KralBuraK3434 <44510005+KralBuraK3434@users.noreply.github.com>
2019-12-07 00:46:22 +0300

    Add files via upload

---
+++ dist/main.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const Request = require("request-promise-native");
+class BWAPIWrapper {
+    constructor(options) {
+        this.BASE_URL = "https://api.bastionbot.org";
+        this.OPTIONS = {
+            json: true,
+        };
+        if (options && typeof options === "object") {
+            Object.assign(this.OPTIONS, options);
+        }
+    }
+    request(path, options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = this.BASE_URL + path;
+            let requestOptions = Object.assign({}, this.OPTIONS);
+            if (options && typeof options === "object") {
+                Object.assign(requestOptions, options);
+            }
+            return yield Request(url, requestOptions);
+        });
+    }
+}
+module.exports = BWAPIWrapper;
+//# sourceMappingURL=main.js.map
---
+++ typings/main.d.ts
@@ -0,0 +1,9 @@
+﻿import * as Request from "request-promise-native";
+declare class BWAPIWrapper {
+    private BASE_URL;
+    private OPTIONS;
+    constructor(options?: Request.RequestPromiseOptions);
+    request(path: string, options?: Request.RequestPromiseOptions): Promise<any>;
+}
+export = BWAPIWrapper;
+//# sourceMappingURL=main.d.ts.map
